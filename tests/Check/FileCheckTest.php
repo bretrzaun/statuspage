a443be47f17264c6f3abdc39b01cd53a892e7acd
ojooss <>
2020-04-16 21:46:02 +0200

    Merged LogFileCheck into FileCheck

--- tests/Check/FileCheckTest.php
+++ tests/Check/FileCheckTest.php
@@ -2,33 +2,104 @@
 namespace BretRZaun\StatusPage\Tests\Check;
 
 use BretRZaun\StatusPage\Check\FileCheck;
+use PHPUnit\Framework\Exception;
 use PHPUnit\Framework\TestCase;
 
-class FileCheckTest extends TestCase
-{
-    public function testFileDoesNotExists(): void
-    {
-        $check = new FileCheck('Test', 'doesnotexist.txt');
-        $result = $check->check();
+class FileCheckTest extends TestCase {
 
+    protected static $testFile;
+
+    public static function setUpBeforeClass(): void
+    {
+        self::$testFile = tempnam(sys_get_temp_dir(), __FUNCTION__);
+        if (false === file_put_contents(
+            self::$testFile,
+            __CLASS__ . PHP_EOL .
+            'This is a test file for unit tests' . PHP_EOL .
+            'And it has to contain: ' . PHP_EOL .
+            '   Success: test passed' . PHP_EOL
+        )) {
+            throw new Exception('Can not write tmp file');
+        }
+    }
+
+    public static function tearDownAfterClass(): void
+    {
+        if (file_exists(self::$testFile)) {
+            unlink(self::$testFile);
+        }
+    }
+
+
+    public function testFileExists()
+    {
+        $check = new FileCheck(__METHOD__, self::$testFile);
+        $result = $check->check();
+        $this->assertTrue($result->getSuccess());
+
+        $check = new FileCheck(__METHOD__, '/file/does/not.exist');
+        $result = $check->check();
         $this->assertFalse($result->getSuccess());
-        $this->assertEquals('doesnotexist.txt does not exist!', $result->getError());
     }
 
-    public function testSuccess(): void
+    public function testAge()
     {
-        $check = new FileCheck('Test', __DIR__.'/../test.log');
+        // file is newer than one minute
+        $check = new FileCheck(__METHOD__, self::$testFile);
+        $check->setMaxage(1);
         $result = $check->check();
-
         $this->assertTrue($result->getSuccess());
+
+        // manipulate timestamp - file should be older
+        $res = touch(self::$testFile, time()-3600,  time()-3600 );
+        $this->assertTrue( $res, 'touch() failed' );
+        // do not forget to clear PHP cache
+        clearstatcache(true, self::$testFile);
+        $result = $check->check();
+        $this->assertFalse($result->getSuccess());
     }
 
-    public function testWritable(): void
+    /**
+     * DataProvider
+     * @return array
+     */
+    public function dataPattern()
     {
-        $check = new FileCheck('Test', __DIR__.'/../test.log');
-        $check->isWritable();
-        $result = $check->check();
-
-        $this->assertTrue($result->getSuccess());
+        return [
+            ['Error', true],
+            ['Error.*Test', true],
+            ['Success:.*passed', false],
+        ];
     }
+
+    /**
+     * @param $pattern
+     * @param $expected
+     * @dataProvider dataPattern
+     */
+    public function testPattern($pattern, $expected)
+    {
+        $check = new FileCheck(__METHOD__, self::$testFile);
+        $check->setUnwantedRegex($pattern);
+        $result = $check->check();
+        $this->assertEquals($expected, $result->getSuccess());
+    }
+
+
+    public function testWriteable()
+    {
+        $check = new FileCheck(__METHOD__, self::$testFile);
+        $check->setWritable();
+        $result = $check->check();
+        $this->assertTrue($result->getSuccess());
+
+        if (posix_getuid() === 0){
+            $this->markTestSkipped('This test can not run as root user');
+        } else {
+            $this->assertTrue(chmod(self::$testFile, 0444), 'Can not modify permissions');
+            $result = $check->check();
+            $this->assertFalse($result->getSuccess());
+        }
+    }
+
 }

2c4c28b7383bff14918eaff94cedeee1229f6e7f
Bret R. Zaun <bretrzaun@hotmail.com>
2021-12-14 22:42:35 +0100

    update dependencies

--- tests/StatusLogTest.php
+++ tests/StatusLogTest.php
@@ -6,14 +6,18 @@ use BretRZaun\StatusPage\Check\CallbackCheck;
 use BretRZaun\StatusPage\Result;
 use PHPUnit\Framework\TestCase;
 use BretRZaun\StatusPage\StatusChecker;
-use Psr\Log\Test\TestLogger;
+use Monolog\Logger;
+use Monolog\Handler\TestHandler;
 
 class StatusLogTest extends TestCase
 {
     public function testLogger(): void
     {
         $statusChecker = new StatusChecker();
-        $logger = new TestLogger();
+        #$logger = new TestLogger();
+        $logger = new Logger('test');
+        $handler = new TestHandler();
+        $logger->pushHandler($handler);
         $statusChecker->setLogger($logger);
 
         $statusChecker->addCheck(new CallbackCheck('Test 1', function($label) {
@@ -26,7 +30,11 @@ class StatusLogTest extends TestCase
         }));
         $statusChecker->check();
 
-        $this->assertTrue($logger->hasAlertRecords());
-        $this->assertEquals('Test 2: this check failed!', $logger->recordsByLevel['alert'][0]['message']);
+        $this->assertTrue($handler->hasAlertRecords());
+        $records = $handler->getRecords();
+        $this->assertEquals('Test 1: OK', $records[0]['message']);
+        $this->assertEquals('INFO', $records[0]['level_name']);
+        $this->assertEquals('Test 2: this check failed!', $records[1]['message']);
+        $this->assertEquals('ALERT', $records[1]['level_name']);
     }
 }

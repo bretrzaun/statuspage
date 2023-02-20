7fdf2623950214733527025ec9aaf6aeb7bdcf28
ojooss <github@o-jooss.de>
2023-02-20 20:54:02 +0100

    add elasticsearch 8 support

--- src/Check/ElasticsearchCheck.php
+++ src/Check/ElasticsearchCheck.php
@@ -2,14 +2,14 @@
 namespace BretRZaun\StatusPage\Check;
 
 use BretRZaun\StatusPage\Result;
-use Elasticsearch\Client;
 use Exception;
+use function PHPUnit\Framework\isEmpty;
 
 class ElasticsearchCheck extends AbstractCheck
 {
 
     /**
-     * @var Client
+     * @var \Elasticsearch\Client|\Elastic\Elasticsearch\Client
      */
     protected $client;
 
@@ -22,12 +22,13 @@ class ElasticsearchCheck extends AbstractCheck
      * Constructor
      *
      * @param string $label
-     * @param Client $client
+     * @param \Elasticsearch\Client|\Elastic\Elasticsearch\Client $client
      * @param array $indices Indices to check for
      */
-    public function __construct(string $label, Client $client, array $indices = [])
+    public function __construct(string $label, \Elasticsearch\Client|\Elastic\Elasticsearch\Client $client, array $indices = [])
     {
         parent::__construct($label);
+
         $this->client = $client;
         $this->indices = $indices;
     }
@@ -41,13 +42,30 @@ class ElasticsearchCheck extends AbstractCheck
     {
         $result = new Result($this->label);
         try {
-            if ($this->client->ping() !== true) {
-                $result->setSuccess(false);
-                return $result;
+            $info = $this->client->info();
+            $versionParts = explode('.', $info['version']['number']);
+            $esMajorVersion = (int)array_shift($versionParts);
+            if ($esMajorVersion >= 8) {
+                if ($this->client->ping()->asBool() !== true) {
+                    $result->setError("Elasticsearch is not reachable (ping failed)");
+                    return $result;
+                }
+            } else {
+                if ($this->client->ping() !== true) {
+                    $result->setError("Elasticsearch is not reachable (ping failed)");
+                    return $result;
+                }
             }
+
             foreach ($this->indices as $index) {
-                if (!$this->client->indices()->exists(['index' => $index])) {
-                    $result->setError("Index '$index' does not exist");
+                if ($esMajorVersion >= 8) {
+                    if (!$this->client->indices()->exists(['index' => $index])->asBool()) {
+                        $result->setError("Index '$index' does not exist");
+                    }
+                } else {
+                    if (!$this->client->indices()->exists(['index' => $index])) {
+                        $result->setError("Index '$index' does not exist");
+                    }
                 }
             }
         } catch (Exception $e) {
--- tests/Check/ElasticsearchCheckTest.php
+++ tests/Check/ElasticsearchCheckTest.php
@@ -2,19 +2,32 @@
 namespace BretRZaun\StatusPage\Tests\Check;
 
 use BretRZaun\StatusPage\Check\ElasticsearchCheck;
-use Elasticsearch\Client;
-use Elasticsearch\Namespaces\IndicesNamespace;
+use DG\BypassFinals;
+use Elastic\Elasticsearch\Client;
+use Elastic\Elasticsearch\Endpoints\Indices;
+use Elastic\Elasticsearch\Response\Elasticsearch;
 use PHPUnit\Framework\TestCase;
+
+// enable mock final classes
+BypassFinals::enable();
 
 class ElasticsearchCheckTest extends TestCase
 {
 
     public function testSuccess(): void
     {
+        /** @noinspection PhpUnitInvalidMockingEntityInspection */
         $client = $this->createMock(Client::class);
         $client->expects($this->once())
-            ->method('ping')
+            ->method('info')
+            ->willReturn(['version' => ['number' => '8.1.2']]);
+        $ping = $this->createMock(Elasticsearch::class);
+        $ping->expects($this->once())
+            ->method('asBool')
             ->willReturn(true);
+        $client->expects($this->any())
+            ->method('ping')
+            ->willReturn($ping);
 
         $check = new ElasticsearchCheck('elasticsearch test', $client);
         $result = $check->checkStatus();
@@ -25,10 +38,18 @@ class ElasticsearchCheckTest extends TestCase
 
     public function testFailure(): void
     {
+        /** @noinspection PhpUnitInvalidMockingEntityInspection */
         $client = $this->createMock(Client::class);
         $client->expects($this->once())
-            ->method('ping')
+            ->method('info')
+            ->willReturn(['version' => ['number' => '8.1.2']]);
+        $ping = $this->createMock(Elasticsearch::class);
+        $ping->expects($this->once())
+            ->method('asBool')
             ->willReturn(false);
+        $client->expects($this->any())
+            ->method('ping')
+            ->willReturn($ping);
 
         $check = new ElasticsearchCheck('elasticsearch test', $client);
         $result = $check->checkStatus();
@@ -38,16 +59,28 @@ class ElasticsearchCheckTest extends TestCase
 
     public function testMissingIndex(): void
     {
+        /** @noinspection PhpUnitInvalidMockingEntityInspection */
         $client = $this->createMock(Client::class);
         $client->expects($this->once())
-            ->method('ping')
+            ->method('info')
+            ->willReturn(['version' => ['number' => '8.1.2']]);
+        $ping = $this->createMock(Elasticsearch::class);
+        $ping->expects($this->once())
+            ->method('asBool')
             ->willReturn(true);
+        $client->expects($this->any())
+            ->method('ping')
+            ->willReturn($ping);
 
-        $incidesMock = $this->createMock(IndicesNamespace::class);
+        $exists = $this->createMock(Elasticsearch::class);
+        $exists->expects($this->once())
+            ->method('asBool')
+            ->willReturn(false);
+        $incidesMock = $this->createMock(Indices::class);
         $incidesMock->expects($this->once())
             ->method('exists')
             ->with(['index' => 'notexisting-test-index'])
-            ->willReturn(false);
+            ->willReturn($exists);
 
         $client->expects($this->once())
             ->method('indices')
